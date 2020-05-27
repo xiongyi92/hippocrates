b5dcbf308bf2cd7ea3483edb967d8ea884ba643a
xiongyi <1170313408@qq.com>
2020-05-27 16:10:26 +0800

    创建

---
+++ main/src/main/java/com/kexuexiong/hippocrates/HippocratesApplication.java
@@ -0,0 +1,13 @@
+package com.kexuexiong.hippocrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippocratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HippocratesApplication.class, args);
+	}
+
+}
---
+++ main/src/test/java/com/kexuexiong/hippocrates/HippocratesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kexuexiong.hippocrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HippocratesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
